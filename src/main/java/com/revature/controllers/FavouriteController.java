fe0644deef3e6b574522ddd69f5646baae27eb67
Andres <joseanpeba@gmail.com>
2022-06-08 20:08:54 +0200

    favoritecontroller

--- src/main/java/com/revature/controllers/FavouriteController.java
+++ src/main/java/com/revature/controllers/FavouriteController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import com.revature.services.SpellService;
 
 @RestController
 @RequestMapping("/favourite")
-
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class FavouriteController {
 
 	
